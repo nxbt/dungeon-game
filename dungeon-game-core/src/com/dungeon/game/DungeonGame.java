f16de4e78946bcf2f2fbaa75e3194438dd969722
EthanBV <ebull-vulpe.sh@mysummitps.org>
2015-10-30 22:54:15 -0700

    Fix for bad sync

--- dungeon-game-core/src/com/dungeon/game/DungeonGame.java
+++ dungeon-game-core/src/com/dungeon/game/DungeonGame.java
@@ -11,39 +11,36 @@ import com.dungeon.game.entity.*;
 import com.dungeon.game.tilemap.Dungeon;
 
 public class DungeonGame extends ApplicationAdapter {
-	
 	Camera cam;
+	Cursor cursor;
 	SpriteBatch batch;
 	Texture img;
 	Entity player;
-	CursorCtrl cursor;
 	
 	@Override
 	public void create() {
-		cursor = new CursorCtrl();
+		Gdx.graphics.setDisplayMode(1280,1024,false);
+		cursor = Gdx.graphics.newCursor(new Pixmap(Gdx.files.internal("Crosshair.png")), 0, 0);
+		
 		cam = new Camera();
 		batch = new SpriteBatch();
 		player = new Player(100,50);
-		
-		Gdx.graphics.setDisplayMode(720, 480, false);
-		
 		Dungeon dungeon = new Dungeon();
 	}
 
 	@Override
 	public void render () {
-		cursor.update();
+		System.out.println(Gdx.graphics.getHeight());
+		Gdx.graphics.setCursor(cursor);
 		
 		player.update();
-		cam.update(player.x, player.y, 1f);
-		
+		cam.update(player.x, player.y, 1f, Gdx.input.getX()-Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2-Gdx.input.getY());
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
 		batch.setProjectionMatrix(cam.getCam().combined);
-		
 		batch.begin();
 		batch.draw(player.sprite, player.x, player.y, 100, 100);
+		batch.draw(new Texture("badlogic.jpg"), 0, 0, 100, 100);
 		batch.end();
 	}
 }
