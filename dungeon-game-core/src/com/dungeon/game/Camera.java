0ae4772f9e7fc632383a04adcc80a6181c423bb6
Brendan Thompson <nxbt1998@gmail.com>
2015-10-31 13:38:59 -0700

    Updated camera

--- dungeon-game-core/src/com/dungeon/game/Camera.java
+++ dungeon-game-core/src/com/dungeon/game/Camera.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 public class Camera {
 	OrthographicCamera cam;
 	
-	private final float TWEEN = 0.2f;
+	private final float TWEEN = 0.3f;
 	
 	private float x;
 	private float y;
@@ -19,8 +19,8 @@ public class Camera {
 	}
 	
 	public void update(float x, float y, float mouseX, float mouseY, float zoom){
-		this.x += ((2*x+(mouseX-Gdx.graphics.getWidth()/2)+this.x)/3 - this.x)*TWEEN;
-		this.y += ((2*y+(mouseY-Gdx.graphics.getHeight()/2)+this.y)/3 - this.y)*TWEEN;
+		this.x += ((3*x+(mouseX-Gdx.graphics.getWidth()/2)+this.x)/4 - this.x)*TWEEN;
+		this.y += ((3*y+(mouseY-Gdx.graphics.getHeight()/2)+this.y)/4 - this.y)*TWEEN;
 		
 		cam.position.set(this.x, this.y, 0);
 		cam.zoom = zoom;
