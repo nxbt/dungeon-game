e89c04e39452308e094791a2cc5d7646b6616e51
EthanBV <ebull-vulpe.sh@mysummitps.org>
2015-10-30 22:48:24 -0700

    Added camera adjustment for the mouse and made fullscreen.

--- dungeon-game-core/src/com/dungeon/game/DungeonGame.java
+++ dungeon-game-core/src/com/dungeon/game/DungeonGame.java
@@ -11,7 +11,6 @@ import com.dungeon.game.entity.*;
 import com.dungeon.game.tilemap.Dungeon;
 
 public class DungeonGame extends ApplicationAdapter {
-	
 	Camera cam;
 	Cursor cursor;
 	SpriteBatch batch;
@@ -20,26 +19,28 @@ public class DungeonGame extends ApplicationAdapter {
 	
 	@Override
 	public void create() {
+		Gdx.graphics.setDisplayMode(1280,1024,false);
 		cursor = Gdx.graphics.newCursor(new Pixmap(Gdx.files.internal("Crosshair.png")), 0, 0);
 		
 		cam = new Camera();
 		batch = new SpriteBatch();
 		player = new Player(100,50);
-		
 		Dungeon dungeon = new Dungeon();
 	}
 
 	@Override
 	public void render () {
+		System.out.println(Gdx.graphics.getHeight());
 		Gdx.graphics.setCursor(cursor);
 		
 		player.update();
-		cam.update(player.x, player.y, 1f);
+		cam.update(player.x, player.y, 1f, Gdx.input.getX()-Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2-Gdx.input.getY());
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.setProjectionMatrix(cam.getCam().combined);
 		batch.begin();
 		batch.draw(player.sprite, player.x, player.y, 100, 100);
+		batch.draw(new Texture("badlogic.jpg"), 0, 0, 100, 100);
 		batch.end();
 	}
 }
--- dungeon-game-desktop/src/com/dungeon/game/desktop/DesktopLauncher.java
+++ dungeon-game-desktop/src/com/dungeon/game/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.dungeon.game.DungeonGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.resizable = false;
 		new LwjglApplication(new DungeonGame(), config);
 	}
 }
