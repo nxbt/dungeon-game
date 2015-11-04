db8e40bc20b9e8e9a67820ff73872a40803be0a4
EthanBV <ebull-vulpe.sh@mysummitps.org>
2015-11-04 08:27:29 -0800

    Continued Expanded TileMap

--- dungeon-game-core/src/com/dungeon/game/world/Tile.java
+++ dungeon-game-core/src/com/dungeon/game/world/Tile.java
@@ -1,5 +1,7 @@
 package com.dungeon.game.world;
 
+import java.util.HashMap;
+
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Tile {
@@ -7,11 +9,16 @@ public class Tile {
 	public int id;
 	public int data;
 	public TextureRegion texture;
-	
+	private static HashMap<Integer, Integer> tileData;
 	public Tile(TextureRegion[] spritesheet, int id) {
 		this.id = id;
 		data = id==2 ? 1:0;
 		
 		texture = spritesheet[id];
+	}
+	
+	private static void initiateTileData(){
+		tileData = new HashMap<Integer, Integer>();
+		tileData.put(0,0);
 	}
 }
