514f189931a82665e4e9189ac0032f1b04b774f8
EthanBV <ebull-vulpe.sh@mysummitps.org>
2015-11-06 21:29:04 -0800

    Revert "Better Generation is complete!"
    This reverts commit fc713d1e87f8bc693819aeacecf8af84c305174f.

--- dungeon-game-core/src/com/dungeon/game/generator/Rooms.java
+++ dungeon-game-core/src/com/dungeon/game/generator/Rooms.java
@@ -10,14 +10,12 @@ public class Rooms {
 	private int height;
 	private int[][] map;
 	private ArrayList<Rectangle> rooms;
-	private ArrayList<ArrayList<int[]>> halls;
 	
 	public Rooms(int width, int height){
 		this.width = width;
 		this.height = height;
 		map = new int[height][width];
 		rooms = new ArrayList<Rectangle>();
-		halls = new ArrayList<ArrayList<int[]>>();
 		generateClearDungeon();
 		int x = height/2;
 		int y = width/2;
@@ -63,7 +61,7 @@ public class Rooms {
 		return false;
 	}
 	
-	public boolean generateBelowRoom(int x, int y, ArrayList<int[]> hall){
+	public boolean generateBelowRoom(int x, int y){
 		int doorX = x;
 		int doorY = y-1;
 		int height = (int) (3+Math.random()*map.length/7);
@@ -73,13 +71,7 @@ public class Rooms {
 		Rectangle room = new Rectangle(x, y, width, height);
 		int nextX;
 		int nextY;
-		halls.add(hall);
 		if(isValidRoom(room)){
-			for(int i=0;i<hall.size();i++){
-				map[hall.get(i)[1]][hall.get(i)[0]]=0;
-				if(i==0)map[hall.get(i)[1]][hall.get(i)[0]]=3;
-				if(i==hall.size()-1)map[hall.get(i)[1]][hall.get(i)[0]]=3;
-			}
 			addRoomToMap(room);
 			for(int i = 0; i<100;i++){
 				int dir = (int) (Math.random()*4);
@@ -103,11 +95,10 @@ public class Rooms {
 			}
 			return true;
 		}
-		halls.remove(halls.size()-1);
 		return false;
 	}
 	
-	public boolean generateAboveRoom(int x, int y, ArrayList<int[]> hall){
+	public boolean generateAboveRoom(int x, int y){
 		int doorX = x;
 		int doorY = y;
 		int height = (int) (3+Math.random()*map.length/7);
@@ -116,13 +107,7 @@ public class Rooms {
 		Rectangle room = new Rectangle(x, y, width, height);
 		int nextX;
 		int nextY;
-		halls.add(hall);
 		if(isValidRoom(room)){
-			for(int i=0;i<hall.size();i++){
-				map[hall.get(i)[1]][hall.get(i)[0]]=0;
-				if(i==0)map[hall.get(i)[1]][hall.get(i)[0]]=3;
-				if(i==hall.size()-1)map[hall.get(i)[1]][hall.get(i)[0]]=3;
-			}
 			addRoomToMap(room);
 			for(int i = 0; i<100;i++){
 				int dir = (int) (Math.random()*4);
@@ -146,11 +131,10 @@ public class Rooms {
 			}
 			return true;
 		}
-		halls.remove(halls.size()-1);
 		return false;
 	}
 	
-	public boolean generateLeftRoom(int x, int y, ArrayList<int[]> hall){
+	public boolean generateLeftRoom(int x, int y){
 		int doorX = x-1;
 		int doorY = y;
 		int height = (int) (3+Math.random()*map.length/7);
@@ -160,13 +144,7 @@ public class Rooms {
 		Rectangle room = new Rectangle(x, y, width, height);
 		int nextX;
 		int nextY;
-		halls.add(hall);
 		if(isValidRoom(room)){
-			for(int i=0;i<hall.size();i++){
-				map[hall.get(i)[1]][hall.get(i)[0]]=0;
-				if(i==0)map[hall.get(i)[1]][hall.get(i)[0]]=3;
-				if(i==hall.size()-1)map[hall.get(i)[1]][hall.get(i)[0]]=3;
-			}
 			addRoomToMap(room);
 			for(int i = 0; i<100;i++){
 				int dir = (int) (Math.random()*4);
@@ -190,11 +168,10 @@ public class Rooms {
 			}
 			return true;
 		}
-		halls.remove(halls.size()-1);
 		return false;
 	}
 	
-	public boolean generateRightRoom(int x, int y, ArrayList<int[]> hall){
+	public boolean generateRightRoom(int x, int y){
 		int doorX = x;
 		int doorY = y;
 		int height = (int) (3+Math.random()*map.length/7);
@@ -203,13 +180,7 @@ public class Rooms {
 		Rectangle room = new Rectangle(x, y, width, height);
 		int nextX;
 		int nextY;
-		halls.add(hall);
 		if(isValidRoom(room)){
-			for(int i=0;i<hall.size();i++){
-				map[hall.get(i)[1]][hall.get(i)[0]]=0;
-				if(i==0)map[hall.get(i)[1]][hall.get(i)[0]]=3;
-				if(i==hall.size()-1)map[hall.get(i)[1]][hall.get(i)[0]]=3;
-			}
 			addRoomToMap(room);
 			for(int i = 0; i<100;i++){
 				int dir = (int) (Math.random()*4);
@@ -233,44 +204,34 @@ public class Rooms {
 			}
 			return true;
 		}
-		halls.remove(halls.size()-1);
 		return false;
 	}
 	
 	public void generateHallWay(int x, int y, int dir){
-		boolean justTurned = true;
 		boolean generateHall = true;
 		boolean generateRoom = true;
 		ArrayList<int[]> hallCoordinates = new ArrayList<int[]>();
 		hallCoordinates.add(new int[]{x,y});
-		int length=5+(int) (Math.random()*15);
+		int length=5+(int) (Math.random()*5);
 		for(int i = 0; i < length; i++){
 			if(dir == 0)y--;
 			if(dir == 1)y++;
 			if(dir == 2)x--;
 			if(dir == 3)x++;
-			if(isValidHallTile(x,y)&&!(hallCoordinates.contains(new int[]{x,y, 0})||hallCoordinates.contains(new int[]{x,y, 1})||hallCoordinates.contains(new int[]{x,y, 2})||hallCoordinates.contains(new int[]{x,y, 3}))){
+			if(isValidHallTile(x,y)&&!hallCoordinates.contains(new int[]{x,y})){
 				hallCoordinates.add(new int[]{x,y,dir});
-				if(Math.random()>0.6&&justTurned==false){
-					justTurned=true;
+				if(Math.random()>0.6){
 					if(dir == 0||dir == 1){
 						dir=2+(int) (Math.random()*2);
 					}else{
 						dir=(int) (Math.random()*2);
 					}
-				}else{
-					justTurned = false;
 				}
 			} else{
-				hallCoordinates.add(new int[]{x,y,dir});
-				if(dir == 0)y--;
-				if(dir == 1)y++;
-				if(dir == 2)x--;
-				if(dir == 3)x++;
-				if(isTileInRoom(x,y)){
+				if(x>0&&y>0&&y<map.length-1&&x<map[0].length-1){
 					i=length;
 					generateRoom = false;
-					generateHall = true;
+					generateHall = false;
 				}else{
 					i=length;
 					generateHall = false;
@@ -279,21 +240,20 @@ public class Rooms {
 			}
 		}
 		if(generateRoom&&generateHall){
-			generateHall=false;
 			if(hallCoordinates.get(hallCoordinates.size()-1)[2] == 0){
-				generateBelowRoom(x, y, hallCoordinates);
+				generateHall=generateBelowRoom(x, y);
 			}else if(hallCoordinates.get(hallCoordinates.size()-1)[2] == 1){
-				generateAboveRoom(x, y+1, hallCoordinates);
+				generateHall=generateAboveRoom(x, y+1);
 			}else if(hallCoordinates.get(hallCoordinates.size()-1)[2] == 2){
-				generateLeftRoom(x, y, hallCoordinates);
+				generateHall=generateLeftRoom(x, y);
 			}else if(hallCoordinates.get(hallCoordinates.size()-1)[2] == 3){
-				generateRightRoom(x+1, y, hallCoordinates);
+				generateHall=generateRightRoom(x+1, y);
 			}
 		}
-		if(generateHall&&Math.random()>0.9&&hallCoordinates.size()>=5){
-			System.out.println(hallCoordinates.size());
-			halls.add(hallCoordinates);
+		if(generateHall){
+			System.out.println("Hall Generated");
 			for(int i=0;i<hallCoordinates.size();i++){
+				System.out.println("Hall Made: "+hallCoordinates.get(i)[0]+", "+hallCoordinates.get(i)[1]);
 				map[hallCoordinates.get(i)[1]][hallCoordinates.get(i)[0]]=0;
 				if(i==0)map[hallCoordinates.get(i)[1]][hallCoordinates.get(i)[0]]=3;
 				if(i==hallCoordinates.size()-1)map[hallCoordinates.get(i)[1]][hallCoordinates.get(i)[0]]=3;
@@ -305,17 +265,6 @@ public class Rooms {
 		boolean result = true;
 		for(Rectangle i: rooms){
 			if(Intersector.intersectRectangles(new Rectangle(room.x-1,room.y-1,room.width+1,room.height+1), new Rectangle(i.x-1,i.y-1,i.width+1,i.height+1), new Rectangle()))result = false;
-		}
-		for(ArrayList<int[]> i: halls){
-			for(int[] k: i){
-				if(!(i==halls.get(halls.size()-1)&&k==i.get(i.size()-1))){
-				boolean xInter = false;
-				boolean yInter = false;
-				if(k[0]>=room.getX()-1&&k[0]<room.getX()+room.getWidth()+1)xInter = true;
-				if(k[1]>=room.getY()-1&&k[1]<room.getY()+room.getHeight()+1)yInter = true;
-				if(xInter&&yInter)result = false;
-				}
-			}
 		}
 		if(room.x<1)result = false;
 		if(room.y<1)result = false;
@@ -332,28 +281,7 @@ public class Rooms {
 			if(x>=i.getX()-1&&x<i.getX()+i.getWidth()+1)xInter = true;
 			if(y>=i.getY()-1&&y<i.getY()+i.getHeight()+1)yInter = true;
 			if(xInter&&yInter)result = false;
-		}
-		if(x<1||y<1||y>map.length-1||x>map[0].length-1)result = false;
-		for(ArrayList<int[]> i: halls){
-			for(int[] k: i){
-				if(x == k[0]&&y==k[1])result = false;
-				if(x+1 == k[0]&&y==k[1])result = false;
-				if(x-1 == k[0]&&y==k[1])result = false;
-				if(x == k[0]&&y+1==k[1])result = false;
-				if(x == k[0]&&y-1==k[1])result = false;
-			}
-		}
-		return result;
-	}
-	
-	public boolean isTileInRoom(int x, int y){
-		boolean result = false;
-		for(Rectangle i: rooms){
-			boolean xInter = false;
-			boolean yInter = false;
-			if(x>=i.getX()&&x<i.getX()+i.getWidth())xInter = true;
-			if(y>=i.getY()&&y<i.getY()+i.getHeight())yInter = true;
-			if(xInter&&yInter)result = true;
+			if(x<1||y<1||y>map.length-1||x>map[0].length-1)result = false;
 		}
 		return result;
 	}
