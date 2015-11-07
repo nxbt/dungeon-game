55a63d24c6d09c9e79eb5b50eb17f282e124c256
Brendan Thompson <nxbt1998@gmail.com>
2015-11-06 22:09:57 -0800

    Fix collision detection bug

--- dungeon-game-core/src/com/dungeon/game/entity/Dynamic.java
+++ dungeon-game-core/src/com/dungeon/game/entity/Dynamic.java
@@ -87,6 +87,35 @@ public abstract class Dynamic extends Entity {
 		boolean ul = world.curFloor.tm[tile_up][tile_lt].data == 1;
 		boolean ur = world.curFloor.tm[tile_up][tile_rt].data == 1;
 		
+		if(dl && dr) {
+			y = (tile_dn+1) * Tile.TS;
+			dy = 0;
+			
+			dl = false;
+			dr = false;
+		}
+		if(ul && ur) {
+			y = (tile_up * Tile.TS)-height;
+			dy = 0;
+			
+			ul = false;
+			ur = false;
+		}
+		if(dl && ul) {
+			x = (tile_lt+1) * Tile.TS;
+			dx = 0;
+			
+			dl = false;
+			ul = false;
+		}
+		if(dr && ur) {
+			x = (tile_rt * Tile.TS)-width;
+			dx = 0;
+			
+			ul = false;
+			ur = false;
+		}
+		
 		if(dl) {
 			if((tile_lt+1)*Tile.TS - this.x < (tile_dn+1)*Tile.TS - this.y) {
 				x = (tile_lt+1) * Tile.TS;
