b97b7745a96a1d4f5f73de577d68f83ff5d8d70a
EthanBV <ebull-vulpe.sh@mysummitps.org>
2015-11-09 21:24:18 -0800

    Drops now jump to open tiles
    Ayyy

--- dungeon-game-core/src/com/dungeon/game/entity/Drop.java
+++ dungeon-game-core/src/com/dungeon/game/entity/Drop.java
@@ -3,6 +3,7 @@ package com.dungeon.game.entity;
 import com.dungeon.game.item.Crap;
 import com.dungeon.game.item.Item;
 import com.dungeon.game.item.Slot;
+import com.dungeon.game.world.Tile;
 import com.dungeon.game.world.World;
 
 public class Drop extends Static {
@@ -27,6 +28,73 @@ public class Drop extends Static {
 
 	@Override
 	public void calc(World world) {
+		int tile_lt = (int) (x/Tile.TS);
+		int tile_dn = (int) (y/Tile.TS);
+		int tile_rt = (int) ((x+Item.SIZE)/Tile.TS);
+		int tile_up = (int) ((y+Item.SIZE)/Tile.TS);
+		
+		boolean dl = world.curFloor.tm[tile_dn][tile_lt].data == 1;
+		boolean dr = world.curFloor.tm[tile_dn][tile_rt].data == 1;
+		boolean ul = world.curFloor.tm[tile_up][tile_lt].data == 1;
+		boolean ur = world.curFloor.tm[tile_up][tile_rt].data == 1;
+		
+		if(dl && dr) {
+			y = (tile_dn+1) * Tile.TS;
+			
+			dl = false;
+			dr = false;
+		}
+		if(ul && ur) {
+			y = (tile_up * Tile.TS)-Item.SIZE;
+			
+			ul = false;
+			ur = false;
+		}
+		if(dl && ul) {
+			x = (tile_lt+1) * Tile.TS;
+			
+			dl = false;
+			ul = false;
+		}
+		if(dr && ur) {
+			x = (tile_rt * Tile.TS)-Item.SIZE;
+			
+			ul = false;
+			ur = false;
+		}
+		
+		if(dl) {
+			if((tile_lt+1)*Tile.TS - this.x < (tile_dn+1)*Tile.TS - this.y) {
+				x = (tile_lt+1) * Tile.TS;
+			}
+			else {
+				y = (tile_dn+1) * Tile.TS;
+			}
+		}
+		if(dr) {
+			if(x+Item.SIZE - tile_rt*Tile.TS < (tile_dn+1)*Tile.TS - this.y) {
+				x = (tile_rt * Tile.TS)-Item.SIZE;
+			}
+			else {
+				y = (tile_dn+1) * Tile.TS;
+			}
+		}
+		if(ul) {
+			if((tile_lt+1)*Tile.TS - this.x < y+Item.SIZE - tile_up*Tile.TS) {
+				x = (tile_lt+1) * Tile.TS;
+			}
+			else {
+				y = (tile_up * Tile.TS)-Item.SIZE;
+			}
+		}
+		if(ur) {
+			if(x+Item.SIZE - tile_rt*Tile.TS < y+Item.SIZE - tile_up*Tile.TS) {
+				x = (tile_rt * Tile.TS)-Item.SIZE;
+			}
+			else {
+				y = (tile_up * Tile.TS)-Item.SIZE;
+			}
+		}
 		if(world.mouse.canPickup && world.mouse.x > x-world.cam.x+world.cam.WIDTH/2 && world.mouse.x < x+Item.SIZE-world.cam.x+world.cam.WIDTH/2 && world.mouse.y > y-world.cam.y+world.cam.HEIGHT/2 && world.mouse.y < y+Item.SIZE-world.cam.y+world.cam.HEIGHT/2) {
 			if(world.mouse.lb_pressed) {
 				if(slot.item != null && world.mouse.slot.item != null && world.mouse.slot.item.name.equals(slot.item.name)) {
--- dungeon-game-core/src/com/dungeon/game/entity/Mouse.java
+++ dungeon-game-core/src/com/dungeon/game/entity/Mouse.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.dungeon.game.item.Crap;
 import com.dungeon.game.item.Item;
