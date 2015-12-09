28495eb2f694b226c728093ff290c736c375f1a3
EthanBV <ebull-vulpe.sh@mysummitps.org>
2015-12-08 22:38:18 -0800

    Added non-hud entity click detection

--- dungeon-game-core/src/com/dungeon/game/entity/hud/Mouse.java
+++ dungeon-game-core/src/com/dungeon/game/entity/hud/Mouse.java
@@ -89,8 +89,19 @@ public class Mouse extends Hud {
 				break;
 			}
 		}
+
 		world.hudEntities.add(0,world.hudEntities.get(toMoveToFront));
 		world.hudEntities.remove(toMoveToFront+1);
+		for(int i = 0; i < world.entities.size(); i++) {
+			Entity ent = world.entities.get(i);
+			if(x > ent.x && x < ent.x+ent.d_width && y > ent.y && y < ent.y+ent.d_height){
+//				if(ent.isClickable==true){
+//					ent.clicked(world);
+					onHud = true;
+					break;
+//				}
+			}
+		}
 		
 		canPickup = (!onHud &&  Math.sqrt(Math.pow((x+world.cam.x-world.cam.WIDTH/2) - (world.player.x + world.player.d_width/2), 2) + Math.pow((y+world.cam.y-world.cam.HEIGHT/2) - (world.player.y + world.player.d_height/2), 2)) <= world.player.REACH);
 		for(int i = 0; i< world.curFloor.tm.length;i++){
