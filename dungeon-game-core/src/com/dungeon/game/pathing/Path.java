6acd14d8be30734a32ec5d8190aa3dc41e5ffa79
EthanBV <ebull-vulpe.sh@mysummitps.org>
2016-01-09 00:51:27 -0800

    Did a bit of pathing

--- dungeon-game-core/src/com/dungeon/game/pathing/Path.java
+++ dungeon-game-core/src/com/dungeon/game/pathing/Path.java
@@ -3,13 +3,19 @@ package com.dungeon.game.pathing;
 import java.util.ArrayList;
 
 public class Path {
+	private int[] start;
+	private int[] end;
 	ArrayList<Area> areas;
-	public Path(Area area){
+	public Path(Area area, int[] start, int[] end){ // constructor used to create initial path
 		areas = new ArrayList<Area>();
 		areas.add(area);
+		this.start = start;
+		this.end = end;
 	}
-	public Path(ArrayList<Area> areas, Area area){
-		this.areas = areas;
+	public Path(Path path, Area area){ //constructor used to create paths
+		this.areas = new ArrayList<Area>(path.areas);
 		areas.add(area);
 	}
+	
+	
 }
