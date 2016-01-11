c2c3930717c92d853cb37f8dcca9593ee7e239e5
Ethan <ebull-vulpe.sh@mysummitps.org>
2016-01-11 09:18:47 -0800

    Update AreaMap.java

--- dungeon-game-core/src/com/dungeon/game/pathing/AreaMap.java
+++ dungeon-game-core/src/com/dungeon/game/pathing/AreaMap.java
@@ -3,10 +3,12 @@ package com.dungeon.game.pathing;
 import java.util.ArrayList;
 
 public class AreaMap {
-	ArrayList<Area> areas; //contains data for all areas;
+	private ArrayList<Area> areas; //contains data for all areas;
+	private Tile[][] tm; //contains tile data for the entire areaMap;
 	
-	public AreaMap(){
+	public AreaMap(Tile[][] tm){
 		areas = new ArrayList<Area>();
+		this.tm = tm;
 	}
 	
 	public addArea(Area area){
