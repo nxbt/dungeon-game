387f93a80aa5957b856be9aaab1355c25f6a52cb
Ethan <ebull-vulpe.sh@mysummitps.org>
2016-01-11 09:08:36 -0800

    Added Some path-finding code.

--- dungeon-game-core/src/com/dungeon/game/pathing/Area.java
+++ dungeon-game-core/src/com/dungeon/game/pathing/Area.java
@@ -6,9 +6,38 @@ import com.dungeon.game.entity.Entity;
 import com.dungeon.game.world.Tile;
 
 public class Area {
-	ArrayList<Area> adjacentAreas; //Holds reference to all adjacent Areas
-	ArrayList<int[][]> Edges; //Holds points where this Area Borders other Areas
-	ArrayList<int[][][][]> minPaths; //Holds information for the minimum viable paths from one edge to another;
-	Tile[][] tm; //contains tile data for the area; 
-	ArrayList<Entity> entities; //contains data for all entities in the area;
+	private ArrayList<int[]> points; //Array of all tiles in this area
+	private ArrayList<int[]> edgePoints; //Array of all edges of this area.
+	private ArrayList<Area> adjacentAreas; //Holds reference to all adjacent Areas
+	private ArrayList<int[][]> Edges; //Holds points where this Area Borders other Areas
+	private ArrayList<int[][][][]> minPaths; //Holds information for the minimum viable paths from one edge to another;
+	private Tile[][] tm; //contains tile data for the area; 
+	private ArrayList<Entity> entities; //contains data for all entities in the area;
+	
+	public Area(){
+		
+	}
+	
+	public void addRectangleToArea(int x, int y, int width, int height){ //adds a rectangle of points to the Area.
+		for(int i = x; i < x+width; i++){
+			for(int k = y; k< y+height; k++){
+				points.add(new int[]{x,y});
+			}
+		}
+		edgePoints = new ArrayList<int[]>();
+		for(int[] i: points){
+			boolean topFound = false;
+			boolean botFound = false;
+			boolean leftFound = false;
+			boolean rightFound = false;
+			for(int[] k: points){
+				if(i[0]+1==k[0]&&i[1]==k[1]) rightFound = true;
+				if(i[0]-1==k[0]&&i[1]==k[1]) leftFound = true;
+				if(i[0]==k[0]&&i[1]+1==k[1]) topFound = true;
+				if(i[0]==k[0]&&i[1]-1==k[1]) botFound = true;
+			}
+			
+			if(!(topFound&&botFound&&leftFound&&rightFound))edgePoints.add(i);
+		}
+	}
 }
