78ba87db56b7784a4276d13c5643097c4a84cf73
Ethan <ebull-vulpe.sh@mysummitps.org>
2016-01-12 09:13:46 -0800

    Added more methods to the new path-finding code!

--- dungeon-game-core/src/com/dungeon/game/pathing/Area.java
+++ dungeon-game-core/src/com/dungeon/game/pathing/Area.java
@@ -10,7 +10,7 @@ public class Area {
 	private ArrayList<int[]> edgePoints; //Array of all edges of this area.
 	private ArrayList<Area> adjacentAreas; //Holds reference to all adjacent Areas
 	private ArrayList<ArrayList<int[]>> edges; //Holds points where this Area Borders other Areas
-	private ArrayList<int[][][][]> minPaths; //Holds information for the minimum viable paths from one edge to another;
+	private ArrayList<ArrayList<ArrayList<ArrayList<int[]>>>> minPaths; //Holds information for the minimum viable paths from one edge to another;
 	private ArrayList<Entity> entities; //contains data for all entities in the area;
 	
 	public Area(){
@@ -18,7 +18,7 @@ public class Area {
 		edgePoints = new ArrayList<int[]>();
 		adjacentAreas = new ArrayList<Area>();
 		edges = new ArrayList<ArrayList<int[]>>();
-		minPaths = new ArrayList<int[][][][]>();
+		minPaths = new ArrayList<ArrayList<ArrayList<ArrayList<int[]>>>>();
 		entities = new ArrayList<Entity>();
 		
 	}
@@ -29,9 +29,9 @@ public class Area {
 				points.add(new int[]{x,y});
 			}
 		}
-		recalculateEdges();
+		calculateEdges();
 	}
-	private void recalculateEdges() { //recalculates the edges of this area;
+	private void calculateEdges() { //recalculates the edges of this area;
 		edgePoints = new ArrayList<int[]>();
 		for(int[] i: points){
 			boolean topFound = false;
@@ -47,5 +47,43 @@ public class Area {
 			
 			if(!(topFound&&botFound&&leftFound&&rightFound))edgePoints.add(i);
 		}
+	}
+	public void calculateBorders(Area a){ //find the borders between this area and another one.
+		ArrayList<int[]> borders = new ArrayList<int>();
+		for(int[] i:edgePoints){
+			for(int[] k: a.edgePoints){
+				if(i[0]-1==k[0]&&i[1]==k[1])borders.add(new int[]{i[0], k[0]});
+				else if(i[0]+1==k[0]&&i[1]==k[1])borders.add(new int[]{i[0], k[0]});
+				else if(i[0]==k[0]&&i[1]-1==k[1])borders.add(new int[]{i[0], k[0]});
+				else if(i[0]==k[0]&&i[1]+1==k[1])borders.add(new int[]{i[0], k[0]});
+			}
+		}
+		if(borders.size()>0){
+			int indexOfArea = 0;
+			if(adjacentAreas.indexOf(a)==-1){
+				adjacentAreas.add(a);
+			}
+			indexOfArea = adjacentAreas.indexOf(a);
+			
+			edges.set(indexOfArea, borders);
+		}
+	}
+	
+	public void calculateMinPaths(){ //calculates the shortest path between any two edges of this area;
+		for(int i = 0; i < edges.size();i++){
+			for(int k = 0; k <edges.size();k++){
+				if(i!=k){
+					for(int[] start: edges.get(i)){
+						for(int[] end: edges.get(k)){
+							//pathfind from start to end and add that to the array minPaths
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	public ArrayList<int[]> findPath(int[] start, int[] end){ //uses A* to find a path within from one edge to another
+		
 	}
 }
