a1428168e4dbb89a9c3c306a5a13ccb2f76cb504
Brendan Thompson <nxbt1998@gmail.com>
2016-01-21 11:32:06 -0800

    Update Immune.java

--- dungeon-game-core/src/com/dungeon/game/effect/Immune.java
+++ dungeon-game-core/src/com/dungeon/game/effect/Immune.java
@@ -4,19 +4,17 @@ import com.dungeon.game.entity.Character;
 
 public class Immune extends Effect {
 	public Immune(int duration) {
-		super("Stun", duration);
+		super("Immune", duration);
 	}
 	
-	public void begin(Character character){
+	public void begin(Character character) {
 		character.immune = true;
 	}
 	
-	public void end(Character character){
+	public void end(Character character) {
 		if(character.immortal)return;
 		for(Effect effect: character.effects){
-			if(effect instanceof Immune&&!effect.equals(this)){
-				return;
-			}
+			if(effect instanceof Immune&&!effect.equals(this)) return;
 		}
 		character.immune = false;
 	}
