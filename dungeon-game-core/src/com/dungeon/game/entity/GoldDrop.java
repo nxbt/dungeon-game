e13cbab79218a276e72eb68ebf3b02961e797b7c
Brendan Thompson <nxbt1998@gmail.com>
2016-02-01 20:41:50 -0800

    continuted to add gold drops
    suck a cock ethan

--- dungeon-game-core/src/com/dungeon/game/entity/GoldDrop.java
+++ dungeon-game-core/src/com/dungeon/game/entity/GoldDrop.java
@@ -1,30 +1,37 @@
 package com.dungeon.game.entity;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.dungeon.game.world.World;
 
 public class GoldDrop extends Static {
-
-	public GoldDrop(World world, float x, float y) {
+	public int amount;
+	
+	public GoldDrop(World world, float x, float y, int amount) {
 		super(world, x, y);
-		// TODO Auto-generated constructor stub
+		
+		sprite = new Texture("coin.png");
+		
+		this.amount = amount;
+		
+		angle = (float) (180f-Math.random()*360);
 	}
 
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
 
 	}
+
+	public void hovered() {
+		if(world.mouse.lb_pressed) {
+			world.player.gold += amount;
+			killMe = true;
+		}
+	}
+	
+	@Override
+	public void calc() {}
 
 	@Override
-	public void calc() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void post() {
-		// TODO Auto-generated method stub
-
-	}
+	public void post() {}
 
 }
