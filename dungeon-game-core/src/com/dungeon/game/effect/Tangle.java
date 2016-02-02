621706ee86a64b800d896ba89f161e664042219e
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-02-02 13:17:35 -0800

    Create Tangle.java

---
+++ dungeon-game-core/src/com/dungeon/game/effect/Tangle.java
@@ -0,0 +1,20 @@
+package com.dungeon.game.effect;
+
+import com.dungeon.game.entity.Character;
+import com.dungeon.game.world.World;
+
+public class Tangle extends Effect {
+	public Immune(World world, int duration) {
+		super(world, "Tangled", duration);
+		texture = new Texture("tangle.png");
+		graphic = new EffectGraphic(world, this);
+	}
+	
+	public String getHoveredText() {
+    		return "Your Tangled for  "+(int)Math.round(duration/60)+" secods!";
+    	}
+    	
+    	public int getNum() {
+    		return (int)duration;
+    	}
+}
