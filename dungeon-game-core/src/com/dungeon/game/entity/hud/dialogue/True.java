13fc7d51ed0aafa570cfdb46dabce5e4beb5a8bc
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-02-05 14:16:00 -0800

    Create True.java

---
+++ dungeon-game-core/src/com/dungeon/game/entity/hud/dialogue/True.java
@@ -0,0 +1,15 @@
+package com.dungeon.game.entity.hud.dialogue;
+
+import com.dungeon.game.world.World;
+
+public class True extends Criteria{
+  
+  
+  public Dialogue(World world{
+    super(world);
+  }
+  
+  public boolean metCriteria(){
+    return true;
+  }
+}
