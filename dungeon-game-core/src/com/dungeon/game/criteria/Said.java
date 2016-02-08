2fd0e3b2180ee41175559068c6231518c32a49ee
Brendan Thompson <nxbt1998@gmail.com>
2016-02-08 15:10:34 -0800

    Removed invert variable from Said.java

--- dungeon-game-core/src/com/dungeon/game/criteria/Said.java
+++ dungeon-game-core/src/com/dungeon/game/criteria/Said.java
@@ -7,24 +7,18 @@ import com.dungeon.game.world.World;
 public class Said extends Criteria{
 	private Dialogue dialogue;
 	private int index;
-	boolean isTrue;
   
-	public Said(World world, Dialogue dialogue, int index, boolean isTrue){
+	public Said(World world, Dialogue dialogue, int index){
 		super(world);
 		this.dialogue = dialogue;
 		this.index = index;
-		this.isTrue = isTrue;
 	}
   
 	public boolean metCriteria(){
 		for(SpeechBubble bubble: dialogue.speechBubbles){
-			if(dialogue.potentialBubbles.indexOf(bubble) == index){
-				if(isTrue)return true;
-				else return false;
-			}
+			if(dialogue.potentialBubbles.indexOf(bubble) == index) return true;
 		}
 		
-		if(isTrue)return false;
-		else return true;
+		return false;
 	}
 }
