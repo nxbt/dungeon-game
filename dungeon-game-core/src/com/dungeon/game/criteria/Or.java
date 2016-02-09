806ec599fcbc119172273bd640a6da3da4731313
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-02-09 12:08:14 -0800

    Create Or.java

---
+++ dungeon-game-core/src/com/dungeon/game/criteria/Or.java
@@ -0,0 +1,20 @@
+package com.dungeon.game.criteria;
+
+import com.dungeon.game.world.World;
+import com.dungeon.game.entity.character.Character;
+
+public class Or extends Criteria {
+  private Criteria[] criterias;
+  
+	public Or(World world, Criteria... criterias){
+		super(world);
+		this.criterias = criterias;
+	}
+  
+	public boolean metCriteria(){
+	  for(Criteria criteria: criterias){
+	    if(criteria.metCriteria())return true;
+	  }
+		return false;
+	}
+}
