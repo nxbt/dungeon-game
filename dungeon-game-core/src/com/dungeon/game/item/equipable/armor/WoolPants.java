bf9c21faa86a37d5ed6a6c88c591c9b17241a42d
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-06-27 21:15:38 -0400

    pants have armor now

--- dungeon-game-core/src/com/dungeon/game/item/equipable/armor/WoolPants.java
+++ dungeon-game-core/src/com/dungeon/game/item/equipable/armor/WoolPants.java
@@ -1,6 +1,7 @@
 package com.dungeon.game.item.equipable.armor;
 
 import com.badlogic.gdx.graphics.Color;
+import com.dungeon.game.effect.Armor;
 import com.dungeon.game.spritesheet.Spritesheet;
 import com.dungeon.game.world.World;
 
@@ -19,6 +20,8 @@ public class WoolPants extends Pants {
 		type = PANTS;
 		
 		desc = "A simple pair of wool pants. Provideds little defence.\n\n Armor: " + (int)physc_resist;
+		
+		passiveEffects.add(new Armor(world, (int) physc_resist));
 	}
 
 }
