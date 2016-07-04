6f420a005da137d6fd5cf02398dd33c6e78ecf98
Brendan Thompson <nxbt1998@gmail.com>
2016-07-03 17:55:04 -0700

    Updated arrow description

--- dungeon-game-core/src/com/dungeon/game/item/ammo/Arrow.java
+++ dungeon-game-core/src/com/dungeon/game/item/ammo/Arrow.java
@@ -18,7 +18,7 @@ public class Arrow extends Ammo {
 		damage = 10;
 		
 		name = "Arrow";
-		desc = "Usually fired from a bow.\n\nDamage: " + damage;
+		desc = "Common ammunition used for bows.\n\nDamage: " + damage;
 		
 		maxStack = 12;
 	}
@@ -43,6 +43,17 @@ public class Arrow extends Ammo {
 	}
 	
 	public String getDesc() {
-		return "A common ammo class item generally consumed by using a bow class weapon. Once fired from a bow class weapon, an arrow item will become an arrow projectile, traveling in a straight line and damaging any target in comes into contact with.\n\n\"White fluff on one end, and black pain on the other.\" -Duryin, bow crafter\n\n Damage: " + damage;
+		return "Arrow\n  A common to find and easy to create projectile, the arrow is what makes the bow the most used projectile weapon. The arrow consists of three major parts; the shaft, "
+				+ "which is a simple wooden stick; the fletching, which is the tuff of feathers near the end, and the head, which is a sharpened black stone. The head is the most difficult "
+				+ "piece to craft, to the point where people will spend their whole lives practicing the art of sharpening heads to perfection. These people can be found in some villages "
+				+ "and towns, where the head of an arrow and be bought cheaply. Once someone has all three pieces to create an arrow, the process of putting them together is simple enough "
+				+ "to be completed at any basic workbench by even an amature.\n  Firing an arrow is a simple matter of latching it to the string of a bow and pulling back on the bow, then "
+				+ "releasing. Arrows are sturdy enough that they will sometimes survive an impact intact, where they can then be retrieved and used again. If an arrow comes in contact with "
+				+ "its target, the sharp head will dig into flesh with ease, inflicting damage porportional to the speed it was launched at.\n  While enchanting arrows is technically possible, "
+				+ "doing so inefficient, and so the art has been long forgotten. Instead, arrows are indirectly imbued by enchanting a bow, which can transfer magical properties to the arrow "
+				+ "while it is notched. While this method of magically enhancing arrows is superior to the alternative in most regards, certin enchantments are impossible with this method. "
+				+ "for example, some enchanters and mages speculate that an enchantment directly on an arrow may be able to produce an arrow that magically returns to the owner after impact. "
+				+ "while many have sought after such an enchantment, little sucsess has been found, and many still doubt that such an enchantment would be possible, especially since the art of "
+				+ "arrow enchantment has been long lost.\n\n\"White fluff on one end, and black pain on the other.\" -Duryin, bow crafter";
 	}
 }
