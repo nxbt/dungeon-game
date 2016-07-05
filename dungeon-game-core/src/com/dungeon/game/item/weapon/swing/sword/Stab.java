56e565c55cabb6e6627050f8d529e2e8f1f24cbd
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-07-05 17:13:10 -0400

    why no commit?

---
+++ dungeon-game-core/src/com/dungeon/game/item/weapon/swing/sword/Stab.java
@@ -0,0 +1,26 @@
+package com.dungeon.game.item.weapon.swing.sword;
+
+import com.dungeon.game.world.World;
+
+public class Stab extends Swing {
+
+	public Stab(World world) {
+		super(world, "Stab");
+	}
+
+	
+	public void setPrevSwing(Swing prevSwing) {
+		this.prevSwing = prevSwing;
+		if(prevSwing.endingZone == LEFT){
+			setStats(false, 15, 12, 30, -7, 4, 28, 6, -3, 1.5f, 0.7f, 0, 0.4f, 0.8f);
+			endingZone = LEFT;
+		}else if(prevSwing.endingZone == CENTER){
+			setStats(false, 15, 12, 0, 0, 4, 28, 0, 0, 1.5f, 0.7f, 0, 0.4f, 1f);
+			endingZone = CENTER;
+		}else if(prevSwing.endingZone == RIGHT){
+			setStats(false, 15, 12, -40, 7, 4, 28, -12, 3, 0.75f, 0.7f, 0, 0.4f, 1.5f);
+			endingZone = RIGHT;
+		}
+	}
+
+}
