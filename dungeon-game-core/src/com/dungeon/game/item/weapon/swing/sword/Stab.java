594f78ef710571494bc4510603c3e42627e77543
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-07-05 19:15:53 -0400

    multiple stabs now function properly

--- dungeon-game-core/src/com/dungeon/game/item/weapon/swing/sword/Stab.java
+++ dungeon-game-core/src/com/dungeon/game/item/weapon/swing/sword/Stab.java
@@ -13,7 +13,14 @@ public class Stab extends SwordSwing {
 	public void setPrevSwing(Swing prevSwing) {
 		this.prevSwing = prevSwing;
 		if(prevSwing instanceof Stab){
-			if(prevSwing.endingZone == LEFT){
+			if((prevSwing.endingZone == RIGHT && prevSwing.prevSwing != null && prevSwing.prevSwing.endingZone == CENTER)){
+				setStats(false, 15, 12, 0, 0, 4, 28, 0, 0, 1.5f, 0.7f, 0, 0.4f, 1f);
+				endingZone = CENTER;
+			}else if((prevSwing.endingZone == CENTER && prevSwing.prevSwing != null && prevSwing.prevSwing.endingZone == RIGHT)){
+				setStats(false, 15, 12, 30, -7, 4, 28, 6, -3, 1.5f, 0.7f, 0, 0.4f, 0.8f);
+				endingZone = LEFT;
+			}
+			else if(prevSwing.endingZone == LEFT){
 				setStats(false, 15, 12, 0, 0, 4, 28, 0, 0, 1.5f, 0.7f, 0, 0.4f, 1f);
 				endingZone = CENTER;
 			}else if(prevSwing.endingZone == CENTER){
