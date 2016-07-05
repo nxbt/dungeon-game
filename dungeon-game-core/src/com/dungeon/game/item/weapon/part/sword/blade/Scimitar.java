030f104cc022757298f3c05abf036a9a5235db0d
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-07-05 17:36:09 -0400

    best laid plans

--- dungeon-game-core/src/com/dungeon/game/item/weapon/part/sword/blade/Scimitar.java
+++ dungeon-game-core/src/com/dungeon/game/item/weapon/part/sword/blade/Scimitar.java
@@ -5,6 +5,7 @@ import com.dungeon.game.world.World;
 public class Scimitar extends SwordBlade {
 	
 	//we all know its real name is SwashBuckler! <3 
+	//due to a type, there will now be a unique item in the game called SwashBucklrrrrrr
 
 	public Scimitar(World world, int level) {
 		super(world, "Scimitar", SPRITES[3], level);
