fdf5602e171554cf2870a5bdd7fcdb64af6bb118
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-07-06 22:14:24 -0400

    defined execute swing, not done yet

---
+++ dungeon-game-core/src/com/dungeon/game/item/equipable/weapon/swing/sword/Execute.java
@@ -0,0 +1,19 @@
+package com.dungeon.game.item.equipable.weapon.swing.sword;
+
+import com.dungeon.game.item.equipable.weapon.swing.Swing;
+import com.dungeon.game.world.World;
+
+public class Execute extends SwordSwing {
+
+	public Execute(World world, String name) {
+		super(world, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setPrevSwing(Swing prevSwing) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
