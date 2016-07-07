2cfd52602f5a3e947e2dd6bace8545ffb30bf719
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-07-07 15:43:38 -0400

    added basicblade for axe, not done yet

---
+++ dungeon-game-core/src/com/dungeon/game/item/equipable/weapon/part/axe/blade/BasicBlade.java
@@ -0,0 +1,19 @@
+package com.dungeon.game.item.equipable.weapon.part.axe.blade;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.dungeon.game.world.World;
+
+public class BasicBlade extends AxeBlade {
+
+	public BasicBlade(World world, String name, Texture sprite, int level) {
+		super(world, name, sprite, level);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setStats(float level) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
