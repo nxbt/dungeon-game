2b1eb1087a1cdaf43bc697494de2490d460279e5
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-07-07 15:38:16 -0400

    started adding parts to axes

---
+++ dungeon-game-core/src/com/dungeon/game/item/equipable/weapon/part/axe/AxePart.java
@@ -0,0 +1,40 @@
+package com.dungeon.game.item.equipable.weapon.part.axe;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.dungeon.game.item.equipable.weapon.part.Part;
+import com.dungeon.game.item.equipable.weapon.swing.sword.SwordSwing;
+import com.dungeon.game.world.World;
+
+public abstract class AxePart extends Part {
+	
+	//parts
+	public static final int BLADE = 0;
+	public static final int TIP = 1;
+	public static final int HANDLE = 2;
+
+	public AxePart(World world, String name, Texture sprite, int level) {
+		super(world, name, sprite, level);
+		type = AXE;
+	}
+	
+	public String getDesc(){
+		String string = name + "\nDamage: " + damage + "\nSpeed: " + speed + "\nKnockback: " + knockback + "\nWeight: " + weight;
+		if(allowedSwings.length > 0){
+			string += "\nAllowed Swings: ";
+			for(int i = 0; i < allowedSwings.length; i++){
+				string+=SwordSwing.getSwingByName(world, allowedSwings[i]).name;
+				if(i != allowedSwings.length - 1)string +=", ";
+			}
+		}
+		if(bannedSwings.length > 0){
+			string += "\nBanned Swings: ";
+			for(int i = 0; i < bannedSwings.length; i++){
+				string+=SwordSwing.getSwingByName(world, bannedSwings[i]).name;
+				if(i != bannedSwings.length - 1)string +=", ";
+			}
+		}
+		string += repeatable?"\nRepeatable":"\nNot Repeatable";
+		return  string;
+	}
+
+}
---
+++ dungeon-game-core/src/com/dungeon/game/item/equipable/weapon/part/axe/blade/AxeBlade.java
@@ -0,0 +1,32 @@
+package com.dungeon.game.item.equipable.weapon.part.axe.blade;
+
+import java.lang.reflect.Constructor;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.dungeon.game.item.equipable.weapon.part.axe.AxePart;
+import com.dungeon.game.utilities.Spritesheet;
+import com.dungeon.game.world.World;
+
+public abstract class AxeBlade extends AxePart {
+	
+	public static final int NUM = 0;
+
+	public static final Texture[] SPRITES = Spritesheet.getSprites("axeBladeMap.png", 32, 32);
+	
+	public static final Constructor<?>[] parts = new Constructor<?>[]{
+//		BasicBlade.class.getConstructors()[0],
+	};
+
+	public AxeBlade(World world, String name, Texture sprite, int level) {
+		super(world, name, sprite, level);
+		part = BLADE;
+		repeatable = false;
+	}
+	
+	public void draw(SpriteBatch batch, float x, float y){
+		font.draw(batch, "Blade: ", x - 70, y + 22);
+		batch.draw(slot, x, y, 32, 32);
+		batch.draw(sprite, x, y, 32, 32);
+	}
+}
