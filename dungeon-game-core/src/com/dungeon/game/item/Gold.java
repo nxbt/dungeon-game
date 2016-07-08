9b63a6c3e08b737676ed363fdf1304ee8dc0a8db
Brendan Thompson <nxbt1998@gmail.com>
2016-07-07 20:12:50 -0700

    added a description to gold coins

--- dungeon-game-core/src/com/dungeon/game/item/Gold.java
+++ dungeon-game-core/src/com/dungeon/game/item/Gold.java
@@ -11,11 +11,14 @@ public class Gold extends Item {
 		
 		maxStack = Integer.MAX_VALUE;
 		
-		name = "Gold";
+		name = "Gold Coin";
 		desc = "Used to buy items and services.";
 	}
 	
 	public String getDesc() {
-		return "A small, round gold coin with a face inscribed on each sides. One face is of a smiling woman with long, flowing hair. The other face is of a stern looking man with a full beard. Although their origin is unknown, coins like this one can be found throughout every dungeon. Can be used to purchase items and services from merchants. Picking up a coin item will add it into your coin purse, where it can no longer be acessed directly.\n\n\"With enough of these, a man can acomplish anything\" -Kurson, fabled merchant";
+		return "Gold Coin\n  A small, round gold coin with a face inscribed on each sides. One face is of a smiling woman with long, flowing hair. The other face is of a stern looking man with "
+				+ "a full beard. Although their origin is unknown, coins like this one can be found throughout every dungeon. Their strange ubiquity has led to them becoming the standard "
+				+ "curreny throughout the dungeons.\n  Nearly all merchants will require these in exchange for goods and services, and will offer them to acquire the same. As such, any "
+				+ "traveler should be sure to have a few on hand at all times, or they may find themselves in a critical state with no way to get help.\n\n\"With enough of these, a man can acomplish anything\" -Kurson, fabled merchant";
 	}
 }
