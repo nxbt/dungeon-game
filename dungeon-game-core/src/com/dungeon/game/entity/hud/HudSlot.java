655de7392343d0e5aa1069196d4b30c2a2b1e1fa
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-07-09 16:30:02 -0400

    now can only consume items when in fight mode from hud slots

--- dungeon-game-core/src/com/dungeon/game/entity/hud/HudSlot.java
+++ dungeon-game-core/src/com/dungeon/game/entity/hud/HudSlot.java
@@ -28,13 +28,13 @@ public class HudSlot extends Hud {
 		super.hovered();
 		if(world.mouse.shift_down) {
 			world.mouse.shift_down = false;
-			slot.hovered();
+			if(!world.player.fightMode)slot.hovered();
 			world.mouse.shift_down = true;
 		}
 		else {
 			if(world.mouse.lb_pressed && world.mouse.slot.item == null && slot.consume(world.player)) return;
-			
-			slot.hovered();
+
+			if(!world.player.fightMode)slot.hovered();
 			world.descBox.text = "";
 		}
 	}
