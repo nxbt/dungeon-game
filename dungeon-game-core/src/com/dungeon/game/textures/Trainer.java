7c25eb54aaa6961b0aff4d390bda12f74fc8186d
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-07-31 15:46:08 -0400

    trainer texture has sword

--- dungeon-game-core/src/com/dungeon/game/textures/Trainer.java
+++ dungeon-game-core/src/com/dungeon/game/textures/Trainer.java
@@ -3,6 +3,8 @@ package com.dungeon.game.textures;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.dungeon.game.item.equipable.weapon.Sword;
+import com.dungeon.game.item.equipable.weapon.part.sword.blade.SwordBlade;
 
 public class Trainer extends Person {
 
@@ -13,6 +15,8 @@ public class Trainer extends Person {
 	
 	private void generateHelmetAndSword(){
 		Pixmap texMap = texture.getTextureData().consumePixmap();
+		
+		//his helmet
 		int rayNum = 500;
 		for(int i = 0; i < rayNum; i++){
 			float angle = (float) (2f*Math.PI*i/rayNum);
@@ -23,8 +27,13 @@ public class Trainer extends Person {
 				texMap.setColor(0.1f+0.2f*k/13f, 0.1f+0.2f*k/13f, 0.1f+0.2f*k/13f, 1);
 				texMap.drawPixel(16+(int)Math.round(Math.cos(angle)*k), 16+(int)Math.round(Math.sin(angle)*k));
 			}
-//			texMap.drawLine(16, 16, 16+(int)(Math.cos(angle)*length), 16+(int)(Math.sin(angle)*length));
 		}
-		texture = new Texture(texMap);
+		
+		//his sword
+		Texture swordTexture = new Sword(null, 1).sprite;
+		if(!swordTexture.getTextureData().isPrepared())swordTexture.getTextureData().prepare();
+		Pixmap swordPixMap = swordTexture.getTextureData().consumePixmap();
+		swordPixMap.drawPixmap(texMap, 1, 0);
+		texture = new Texture(swordPixMap);
 	}
 }
