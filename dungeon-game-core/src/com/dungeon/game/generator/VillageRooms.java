4e6758612bc72c5bded1d7d32e5ea6435ac8716f
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-03 20:43:31 -0400

    ported all rooms to room classes

--- dungeon-game-core/src/com/dungeon/game/generator/VillageRooms.java
+++ dungeon-game-core/src/com/dungeon/game/generator/VillageRooms.java
@@ -2,22 +2,16 @@ package com.dungeon.game.generator;
 
 import java.util.ArrayList;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 import com.dungeon.game.entity.Entity;
 import com.dungeon.game.entity.character.friend.Trainer;
-import com.dungeon.game.entity.character.friend.Villager;
-import com.dungeon.game.entity.furniture.Bed;
-import com.dungeon.game.entity.furniture.Carpet;
-import com.dungeon.game.entity.furniture.Dresser;
-import com.dungeon.game.entity.furniture.Fireplace;
-import com.dungeon.game.entity.furniture.SmallTable;
 import com.dungeon.game.entity.furniture.Stair;
 import com.dungeon.game.generator.room.GeneralStore;
 import com.dungeon.game.generator.room.Quarters;
 import com.dungeon.game.generator.room.Room;
 import com.dungeon.game.generator.room.StairRoom;
+import com.dungeon.game.generator.room.TrainingCenter;
 import com.dungeon.game.pathing.Area;
 import com.dungeon.game.world.Tile;
 import com.dungeon.game.world.World;
@@ -569,14 +563,17 @@ public class VillageRooms extends Generation {
 		Room storeRoom = new GeneralStore(world, storeRect, storeDoorFinder, tileMap);
 		storeRoom.addToMap(map, entities);
 		
-		generateTrainingCenter(specialRooms.get((int) (specialRooms.size()*Math.random())));
+		Rectangle trainingRect = specialRooms.remove((int) (specialRooms.size()*Math.random()));
+		int[] trainingDoorFinder = findDoor(trainingRect);
+		Room trainingRoom = new TrainingCenter(world, trainingRect, trainingDoorFinder, tileMap);
+		trainingRoom.addToMap(map, entities);
+		
 		while(specialRooms.size()>0){
 			
 			Rectangle quartersRect = specialRooms.remove((int) (specialRooms.size()*Math.random()));
 			int[] quartersDoorFinder = findDoor(quartersRect);
 			Room quartersRoom = new Quarters(world, quartersRect, quartersDoorFinder, tileMap);
 			quartersRoom.addToMap(map, entities);
-//			generateQuarters(specialRooms.get((int) (specialRooms.size()*Math.random())));
 		}
 	}
 	
@@ -596,8 +593,6 @@ public class VillageRooms extends Generation {
 	//other decerative entities (sculptures, etc)
 	
 	private int[] findDoor(Rectangle room){
-		int doorside;
-		int doorpos;
 		int x,y;
 		x = (int) room.x-1;
 		y = (int) room.y;
@@ -641,356 +636,5 @@ public class VillageRooms extends Generation {
 		}
 		
 		return null;
-		
-	}
-	
-	private void generateQuarters(Rectangle room){
-		specialRooms.remove(room);
-		
-		//begining Tranaformations
-		int[] doorFinder = findDoor(room);
-		int[] doorPos = new int[2];
-		int[][] roomMap = rotate(room, doorFinder, doorPos);
-		int doorX =  doorPos[0];
-		int doorY = doorPos[1];
-		
-		//initialize Entity Arrays
-		ArrayList<Entity> roomEntities = new ArrayList<Entity>();
-		
-		//spawn stuff
-		int x = 0,y = 0;
-		ArrayList<int[]> occupiedTiles = new ArrayList<int[]>();
-		
-		roomEntities.add(new Villager(world,1*Tile.TS,1*Tile.TS));
-		
-		occupiedTiles.add(new int[]{doorY,0});
-		occupiedTiles.add(new int[]{doorY,1});
-		occupiedTiles.add(new int[]{doorY+1,0});
-		occupiedTiles.add(new int[]{doorY-1,0});
-		 //spawn bed
-		int bedOrientation = (int)(Math.random()*4);
-		int bedX = 0, bedY = 0;
-		if(bedOrientation == 0 && roomMap.length < 3)bedOrientation = (int)(Math.random()*3+1);
-		if(bedOrientation == 0){
-			x = 0;
-			do{
-				y = (int) (Math.random()*roomMap.length);
-			}while(y == doorY || y - 1 == doorY || y + 1 == doorY);
-			bedX = x;
-			bedY = y;
-			x*=Tile.TS;
-			x+=Tile.TS;
-			y*=Tile.TS;
-			y+=Tile.TS/2;
-			occupiedTiles.add(new int[]{bedY,bedX});
-			occupiedTiles.add(new int[]{bedY,bedX+1});
-			occupiedTiles.add(new int[]{bedY+1,bedX+1});
-			occupiedTiles.add(new int[]{bedY-1,bedX+1});
-		}else if(bedOrientation == 1){
-			x = (int) (Math.random()*roomMap[0].length);
-			if(doorY < 2){
-				while (x < 2){
-					x = (int) (Math.random()*roomMap[0].length);
-				}
-			}
-			y = 0;
-			bedX = x;
-			bedY = y;
-			x*=Tile.TS;
-			x+=Tile.TS/2;
-			y*=Tile.TS;
-			y+=Tile.TS;
-			occupiedTiles.add(new int[]{bedY,bedX});
-			occupiedTiles.add(new int[]{bedY+1,bedX});
-			occupiedTiles.add(new int[]{bedY+1,bedX+1});
-			occupiedTiles.add(new int[]{bedY+1,bedX-1});
-		}else if(bedOrientation == 2){
-			x = roomMap[0].length-1;
-			y = (int) (Math.random()*roomMap.length);
-			bedX = x;
-			bedY = y;
-			x*=Tile.TS;
-			y*=Tile.TS;
-			y+=Tile.TS/2;
-			occupiedTiles.add(new int[]{bedY,bedX});
-			occupiedTiles.add(new int[]{bedY,bedX-1});
-			occupiedTiles.add(new int[]{bedY+1,bedX-1});
-			occupiedTiles.add(new int[]{bedY-1,bedX-1});
-		}else if(bedOrientation == 3){
-			x = (int) (Math.random()*roomMap[0].length);
-			if(doorY > roomMap.length - 3){
-				while (x < 2){
-					x = (int) (Math.random()*roomMap[0].length);
-				}
-			}
-			y = roomMap.length-1;
-			bedX = x;
-			bedY = y;
-			x*=Tile.TS;
-			x+=Tile.TS/2;
-			y*=Tile.TS;
-			occupiedTiles.add(new int[]{bedY,bedX});
-			occupiedTiles.add(new int[]{bedY-1,bedX});
-			occupiedTiles.add(new int[]{bedY-1,bedX+1});
-			occupiedTiles.add(new int[]{bedY-1,bedX-1});
-		}
-		if(bedOrientation == 0)bedOrientation = 3;
-		else if(bedOrientation == 1)bedOrientation = 2;
-		else if(bedOrientation == 2)bedOrientation = 1;
-		else if(bedOrientation == 3)bedOrientation = 0;
-		roomEntities.add(new Bed(world, x, y, bedOrientation));
-		
-		//spawn side table
-		ArrayList<int[]> potentialTableSpots = new ArrayList<int[]>();
-		if(bedOrientation % 2 == 0){
-			potentialTableSpots.add(new int[]{bedY,bedX+1});
-			potentialTableSpots.add(new int[]{bedY,bedX-1});
-		}else{
-			potentialTableSpots.add(new int[]{bedY+1,bedX});
-			potentialTableSpots.add(new int[]{bedY-1,bedX});
-			
-		}
-		
-		for(int i = 0; i < potentialTableSpots.size(); i++){
-			if(potentialTableSpots.get(i)[0] < 0 || potentialTableSpots.get(i)[0] > roomMap.length - 1 || potentialTableSpots.get(i)[1] < 0 || potentialTableSpots.get(i)[1] > roomMap[0].length - 1 || roomMap[potentialTableSpots.get(i)[0]][potentialTableSpots.get(i)[1]] != 0){
-				potentialTableSpots.remove(i);
-				i--;
-			}
-		}
-		int tableSpot = (int) Math.random()*potentialTableSpots.size();
-		
-		roomEntities.add(new SmallTable(world,potentialTableSpots.get(tableSpot)[1]*Tile.TS+Tile.TS/2,potentialTableSpots.get(tableSpot)[0]*Tile.TS+Tile.TS/2));
-		
-		occupiedTiles.add(potentialTableSpots.get(tableSpot));
-		
-		//spawn dresser
-		int attempts = 0;
-		boolean placedDresser = false;
-		int[] dresserPos = new int[]{0,0};
-		int dresserOrientation = 0;
-		do{
-			attempts++;
-			dresserOrientation = (int)(Math.random()*4);
-			if(dresserOrientation == 0){
-				for(int i = 0; i < roomMap.length-1; i++){
-					if(!checkOccupied(occupiedTiles, 0, i)&&!checkOccupied(occupiedTiles, 0, i+1)){
-						placedDresser = true;
-						dresserPos = new int[]{i,0};
-						occupiedTiles.add(dresserPos);
-						occupiedTiles.add(new int[]{i+1,0});
-						break;
-					}
-				}
-			}else if(dresserOrientation == 1){
-				for(int i = 0; i < roomMap[0].length-1; i++){
-					if(!checkOccupied(occupiedTiles, i, 0)&&!checkOccupied(occupiedTiles, i+1, 0)){
-						placedDresser = true;
-						dresserPos = new int[]{0,i};
-						occupiedTiles.add(dresserPos);
-						occupiedTiles.add(new int[]{0,i+1});
-						break;
-					}
-				}
-			}else if(dresserOrientation == 2){
-				for(int i = 0; i < roomMap.length-1; i++){
-					if(!checkOccupied(occupiedTiles, roomMap[0].length-1, i)&&!checkOccupied(occupiedTiles, roomMap[0].length-1, i+1)){
-						placedDresser = true;
-						dresserPos = new int[]{i,roomMap[0].length-1};
-						occupiedTiles.add(dresserPos);
-						occupiedTiles.add(new int[]{i+1,roomMap[0].length-1});
-						break;
-					}
-				}
-			}else{
-				for(int i = 0; i < roomMap[0].length-1; i++){
-					if(!checkOccupied(occupiedTiles, i, roomMap.length-1)&&!checkOccupied(occupiedTiles, i+1, roomMap.length-1)){
-						placedDresser = true;
-						dresserPos = new int[]{roomMap.length-1,i};
-						occupiedTiles.add(dresserPos);
-						occupiedTiles.add(new int[]{roomMap.length-1,i+1});
-						break;
-					}
-				}
-			}
-		}while(!placedDresser&&attempts < 10);
-		
-		roomEntities.add(new Dresser(world, (dresserPos[1]+(dresserOrientation%2==0?0.5f:1))*Tile.TS, (dresserPos[0]+(dresserOrientation%2==0?1:0.5f))*Tile.TS, dresserOrientation+1));
-		roomMap[dresserPos[0]][dresserPos[1]] = 5;
-		if(dresserOrientation % 2 == 0)roomMap[dresserPos[0]+1][dresserPos[1]] = 5;
-		else roomMap[dresserPos[0]][dresserPos[1]+1] = 5;
-		
-		//spawn Fireplace
-		attempts = 0;
-		boolean placedFirepalce = false;
-		int[] fireplacePos = new int[]{0,0};
-		int fireplaceOrientation = 0;
-		do{
-			attempts++;
-			fireplaceOrientation = (int)(Math.random()*4);
-			if(fireplaceOrientation == 0){
-				for(int i = 1; i < roomMap.length-1; i++){
-					if(!checkOccupied(occupiedTiles, 0, i)){
-						placedFirepalce = true;
-						fireplacePos = new int[]{i,0};
-						occupiedTiles.add(fireplacePos);
-						fireplacePos[1]--;
-						break;
-					}
-				}
-			}else if(fireplaceOrientation == 1){
-				for(int i = 1; i < roomMap[0].length-1; i++){
-					if(!checkOccupied(occupiedTiles, i, 0)){
-						placedFirepalce = true;
-						fireplacePos = new int[]{0,i};
-						occupiedTiles.add(fireplacePos);
-						fireplacePos[0]--;
-						break;
-					}
-				}
-			}else if(fireplaceOrientation == 2){
-				for(int i = 1; i < roomMap.length-1; i++){
-					if(!checkOccupied(occupiedTiles, roomMap[0].length-1, i)){
-						placedFirepalce = true;
-						fireplacePos = new int[]{i,roomMap[0].length-1};
-						occupiedTiles.add(fireplacePos);
-						fireplacePos[1]++;
-						break;
-					}
-				}
-			}else{
-				for(int i = 1; i < roomMap[0].length-1; i++){
-					if(!checkOccupied(occupiedTiles, i, roomMap.length-1)){
-						placedFirepalce = true;
-						fireplacePos = new int[]{roomMap.length-1,i};
-						occupiedTiles.add(fireplacePos);
-						fireplacePos[0]++;
-						break;
-					}
-				}
-			}
-		}while(!placedFirepalce&&attempts < 10);
-		
-		roomEntities.add(new Fireplace(world, fireplacePos[1]*Tile.TS+Tile.TS/2, fireplacePos[0]*Tile.TS+Tile.TS/2, fireplaceOrientation == 0?3:fireplaceOrientation == 1?2:fireplaceOrientation == 2?1:0));
-		
-		//spawn Carpet
-		roomEntities.add(new Carpet(world,roomMap[0].length/2f*Tile.TS,roomMap.length/2f*Tile.TS,roomMap[0].length*2-2,roomMap.length*2-2, new Color((float)Math.random(),(float)Math.random(),(float)Math.random(),0.5f)));
-		
-		
-		
-		//ending transformations
-		unrotate(roomMap, room, roomEntities, doorFinder);
-	}
-	
-	
-	//generate the trainer and his shit
-	private void generateTrainingCenter(Rectangle room){
-		specialRooms.remove(room);
-		
-		//begining Tranaformations
-		int[] doorFinder = findDoor(room);
-		int[] doorPos = new int[2];
-		int[][] roomMap = rotate(room, doorFinder, doorPos);
-		int doorX =  doorPos[0];
-		int doorY = doorPos[1];
-		
-		//initialize Entity Arrays
-		ArrayList<Entity> roomEntities = new ArrayList<Entity>();
-		
-		//spawn stuff
-		int x = 0,y = 0;
-		ArrayList<int[]> occupiedTiles = new ArrayList<int[]>();
-		
-		//change floor to tiles
-		for(int i = 0; i < roomMap.length; i++){
-			for(int k = 0; k < roomMap[i].length; k++){
-				roomMap[i][k] = 3;
-			}
-		}
-		
-		//generate things!
-		roomEntities.add(new Trainer(world, Tile.TS, Tile.TS));
-		//ending transformations
-		unrotate(roomMap, room, roomEntities, doorFinder);
-	}
-	
-	
-	private int[][] rotate(Rectangle room, int[] doorFinder, int[] doorPos){
-		int[][] roomMap;
-		if(doorFinder[0]==2||doorFinder[0]==3){
-			roomMap = new int[(int) room.width][(int) room.height];
-			doorPos[0] = 0;
-			doorPos[1] = (int) (doorFinder[1]-room.x);
-		}
-		else {
-			roomMap = new int[(int) room.height][(int) room.width];
-			doorPos[0] = 0;
-			doorPos[1] = (int) (doorFinder[2]- room.y);
-		}
-		return roomMap;
-	}
-	
-	private void unrotate(int[][] roomMap, Rectangle room, ArrayList<Entity> roomEntities, int[] doorFinder){
-		if(doorFinder[0]==1||doorFinder[0]==3){
-			int[][] temp = roomMap.clone();
-			
-			if(doorFinder[0]==1) roomMap = new int[(int) room.height][(int) room.width];
-			else roomMap = new int[(int) room.width][(int) room.height];
-			
-			for(int i = 0; i < temp.length; i++){
-				for(int k = 0; k < temp[i].length; k++){
-					roomMap[i][roomMap[i].length-1-k]=temp[i][k];
-				}
-			}
-			
-			for(Entity e: roomEntities) {
-				e.x = roomMap[0].length*Tile.TS-e.x;
-				e.flipX  = !e.flipX;
-			}
-		}
-		
-		if(doorFinder[0]==2||doorFinder[0]==3){
-			int[][] temp = roomMap.clone();
-			roomMap = new int[(int) room.height][(int) room.width];
-			for(int i = 0; i < temp.length; i++){
-				for(int k = 0; k < temp[i].length; k++){
-					roomMap[k][i]=temp[i][k];
-				}
-			}
-			for(Entity e: roomEntities) {
-				float tempX = e.x;
-				e.x = e.y;
-				e.y = tempX;
-				e.angle-=90;
-				e.flipX = !e.flipX;
-			}
-		}
-		
-		//copy tiles to the map
-		
-		int x = (int) room.x;
-		int y = (int) room.y;
-		
-		for(int i = 0; i < room.height; i++){
-			for(int k = 0; k < room.width; k++){
-				map[y][x] = tileMap.getTile(roomMap[i][k]);
-				x++;
-			}
-			y++;
-			x = (int) room.x;
-		}
-		
-		for(Entity e: roomEntities) {
-			e.x += room.x*Tile.TS;
-			e.y += room.y*Tile.TS;
-			entities.add(e);
-		}
-	}
-	
-	private boolean checkOccupied(ArrayList<int[]> occupied, int x, int y){
-		for(int i = 0; i < occupied.size(); i++){
-			if(x == occupied.get(i)[1] && y == occupied.get(i)[0])return true;
-		}
-		return false;
-		
 	}
 }
