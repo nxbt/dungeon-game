e297b4d5630dbb0c775504160953babe89e78322
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-09 22:18:13 -0700

    rooms in villages are now lit up, but poorly, occupiedTiles not workin

--- dungeon-game-core/src/com/dungeon/game/generator/VillageRooms.java
+++ dungeon-game-core/src/com/dungeon/game/generator/VillageRooms.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 import com.dungeon.game.entity.furniture.Stair;
-import com.dungeon.game.generator.room.Alter;
+import com.dungeon.game.generator.room.BasicRoom;
 import com.dungeon.game.generator.room.GeneralStore;
 import com.dungeon.game.generator.room.Quarters;
 import com.dungeon.game.generator.room.Room;
@@ -561,7 +561,7 @@ public class VillageRooms extends Generation {
 		while(normRooms.size()>0){
 			Rectangle altersRect = normRooms.remove((int) (specialRooms.size()*Math.random()));
 			int[][] altersDoorFinder = findDoors(altersRect);
-			Room altersRoom = new Alter(world, altersRect, altersDoorFinder, tileMap);
+			Room altersRoom = new BasicRoom(world, altersRect, altersDoorFinder, tileMap);
 			altersRoom.addToMap(map, entities);
 		}
 		
---
+++ dungeon-game-core/src/com/dungeon/game/generator/room/BasicRoom.java
@@ -0,0 +1,69 @@
+package com.dungeon.game.generator.room;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Rectangle;
+import com.dungeon.game.entity.furniture.Carpet;
+import com.dungeon.game.entity.furniture.Torch;
+import com.dungeon.game.world.Tile;
+import com.dungeon.game.world.TileMap;
+import com.dungeon.game.world.World;
+
+public class BasicRoom extends Room {
+	int[][] doorPos;
+
+	public BasicRoom(World world, Rectangle roomBase, int[][] doorFinder, TileMap tileMap) {
+		super(world, roomBase, doorFinder, tileMap);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void generate() {
+		doorPos = new int[doorFinder.length][2];
+		for(int i = 0; i < doorFinder.length; i++){
+			doorPos[i][0] = doorFinder[i][1];
+			doorPos[i][1] = doorFinder[i][2];
+			addToOccupied(doorPos[i][0]+1, doorPos[i][1]);
+			addToOccupied(doorPos[i][0]-1, doorPos[i][1]);
+			addToOccupied(doorPos[i][0], doorPos[i][1]+1);
+			addToOccupied(doorPos[i][0], doorPos[i][1]-1);
+		}
+		int x, y;
+		x = 0;
+		for(int i = 0; i < room.length; i++){
+			y = i;
+			if(!checkOccupied(x, y) && Math.random() < 0.1){
+				entities.add(new Torch(world, x*Tile.TS+Tile.TS/4, y*Tile.TS+Tile.TS/2,0));
+				addToOccupied(x, y);
+			}
+		}
+		x = room[0].length-1;
+		for(int i = 0; i < room.length; i++){
+			y = i;
+			if(!checkOccupied(x, y) && Math.random() < 0.1){
+				entities.add(new Torch(world, x*Tile.TS+Tile.TS*3/4, y*Tile.TS+Tile.TS/2,2));
+				addToOccupied(x, y);
+			}
+		}
+		y = 0;
+		for(int i = 0; i < room[0].length; i++){
+			x = i;
+			if(!checkOccupied(x, y) && Math.random() < 0.1){
+				entities.add(new Torch(world, x*Tile.TS+Tile.TS/2, y*Tile.TS+Tile.TS/4,3));
+				addToOccupied(x, y);
+			}
+		}
+		y = room.length-1;
+		for(int i = 0; i < room[0].length; i++){
+			x = i;
+			if(!checkOccupied(x, y) && Math.random() < 0.1){
+				entities.add(new Torch(world, x*Tile.TS+Tile.TS/2, y*Tile.TS+Tile.TS*3/4,1));
+				addToOccupied(x, y);
+			}
+		}
+		entities.add(new Carpet(world,room[0].length/2f*Tile.TS,room.length/2f*Tile.TS,room[0].length*2-2,room.length*2-2, new Color((float)Math.random(),(float)Math.random(),(float)Math.random(),0.5f)));
+		
+		
+		
+	}
+
+}
