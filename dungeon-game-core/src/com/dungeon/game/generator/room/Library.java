530a5e86810b4142a155db9635981deeecccfd00
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-13 23:21:17 -0700

    made the libarary class dont want to math atm

---
+++ dungeon-game-core/src/com/dungeon/game/generator/room/Library.java
@@ -0,0 +1,20 @@
+package com.dungeon.game.generator.room;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.dungeon.game.world.TileMap;
+import com.dungeon.game.world.World;
+
+public class Library extends Room {
+
+	public Library(World world, Rectangle roomBase, int[][] doorFinder, TileMap tileMap) {
+		super(world, roomBase, doorFinder, tileMap);
+		
+	}
+
+	@Override
+	public void generate() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
