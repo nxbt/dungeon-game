286798560ab7ff7d4b768bcd6482d727a016ec3e
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-16 09:50:52 -0700

    started bookshelf shiit

--- dungeon-game-core/src/com/dungeon/game/textures/entity/Bookshelf.java
+++ dungeon-game-core/src/com/dungeon/game/textures/entity/Bookshelf.java
@@ -3,15 +3,26 @@ package com.dungeon.game.textures.entity;
 import com.dungeon.game.textures.ProceduralTexture;
 
 public class Bookshelf extends ProceduralTexture {
+		private static final int NORM_SHELF = 0;
+		private static final int END_SHELF = 1;
+		private static final int CORNER_SHELF = 2;
+		
+		private int width;
+		private int height;
 
-	public Bookshelf(int width, int height, int[] args) {
-		super(16*width, 16*height, args);
+	public Bookshelf(int width, int height, int[][][] shelfMap) {
+		super(16*width, 16*height, getShelfMap1DArray(width, height, shelfMap));
 		
 	}
 
 	@Override
 	public void generateTexture(int[] args) {
 
+	}
+	
+	private void getShelfMap1DArray(int width, int height, int[][][] shelfMap){
+		this.width = width;
+		this.height = height;
 	}
 
 }
