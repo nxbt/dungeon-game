ab8f89a9e851e30988cd5877c26e165f46140bb8
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-15 22:38:18 -0700

    started bookshelf

---
+++ dungeon-game-core/src/com/dungeon/game/textures/entity/Bookshelf.java
@@ -0,0 +1,17 @@
+package com.dungeon.game.textures.entity;
+
+import com.dungeon.game.textures.ProceduralTexture;
+
+public class Bookshelf extends ProceduralTexture {
+
+	public Bookshelf(int width, int height, int[] args) {
+		super(16*width, 16*height, args);
+		
+	}
+
+	@Override
+	public void generateTexture(int[] args) {
+
+	}
+
+}
