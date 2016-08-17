48c15fcbe9c4124a1947f7276f0699d45906825d
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-17 10:38:50 -0700

    generateChecks will hopefully? work

--- dungeon-game-core/src/com/dungeon/game/textures/entity/Carpet.java
+++ dungeon-game-core/src/com/dungeon/game/textures/entity/Carpet.java
@@ -59,6 +59,26 @@ public class Carpet extends ProceduralTexture {
 	public void generateChecks(Pixmap texMap, int width, int height){
 		Color color1 = new Color((float)(0.3f+0.3f*Math.random()), (float)(0.3f+0.3f*Math.random()), (float)(0.3f+0.3f*Math.random()), 1);
 		Color color2 = new Color((float)(0.3f+0.3f*Math.random()), (float)(0.3f+0.3f*Math.random()), (float)(0.3f+0.3f*Math.random()), 1);
+		int noiseSeed = (int)(Math.random()*1000);
+		for(int i = 0; i < width; i++){
+			for(int k = 0; k < height; k++){
+				int xZone = i/16;
+				int yZone = k/16;
+				int check = (xZone+yZone)%2==0?0:1;
+				Color activeColor;
+				if(check == 0)activeColor = new Color(color1.r, color1.g, color1.b, 1);
+				else activeColor = new Color(color2.r, color2.g, color2.b, 1);
+				if(i % 16 == 0 || i % 16 == 15 || k % 16 == 0 || k % 16 == 15){
+					activeColor.r*=0.6f;
+					activeColor.g*=0.6f;
+					activeColor.b*=0.6f;
+				}
+				float num = MathUtils.reductiveNoise2d(noiseSeed, i, k, new float[]{8,4,2,1}, new float[]{4,3,2,1});
+				texMap.setColor(new Color(activeColor.r*(0.9f+0.2f*num), activeColor.g*(0.9f+0.2f*num), activeColor.b*(0.9f+0.2f*num), 1));
+				texMap.drawPixel(i, height - 1 - k);
+			}
+		}
+		
 	}
 	
 	public void generateOrnimental(Pixmap texMap, int width, int height){
