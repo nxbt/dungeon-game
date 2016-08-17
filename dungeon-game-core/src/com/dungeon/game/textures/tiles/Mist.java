b4cbe8dd3c06ed2f0079a7bdbb4a17a1071558c2
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-17 11:26:56 -0700

    Update Mist.java

--- dungeon-game-core/src/com/dungeon/game/textures/tiles/Mist.java
+++ dungeon-game-core/src/com/dungeon/game/textures/tiles/Mist.java
@@ -1,7 +1,17 @@
 package com.dungeon.game.textures.tiles;
 
 public class Dirt extends ProceduralTile {
-  public Dirt(int seed, int x, int y) {
+	public Dirt(int seed, int x, int y) {
 		super(new Object[]{seed, x, y});
+	}
+	
+	
+	public void generateTexture(Object[] args) {
+		
+	}
+	
+	
+	public void update(){
+		
 	}
 }
