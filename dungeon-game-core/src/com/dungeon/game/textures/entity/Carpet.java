97ecea38b4e4dcb2fa5952ae971bef712b873193
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-17 10:17:33 -0700

    Update Carpet.java

--- dungeon-game-core/src/com/dungeon/game/textures/entity/Carpet.java
+++ dungeon-game-core/src/com/dungeon/game/textures/entity/Carpet.java
@@ -40,15 +40,25 @@ public class Carpet extends ProceduralTexture {
 	
 	public void generatePlain(Pixmap texMap, int width, int height){
 		Color carpetColor = new Color((float)(0.3f+0.3f*Math.random()), (float)(0.3f+0.3f*Math.random()), (float)(0.3f+0.3f*Math.random()), 1);
+		int noiseSeed = (int)(Math.random()*1000);
 		for(int i = 0; i < width; i++){
 			for(int k = 0; k < height; k++){
-				
+				if(i == 0 || k == 0 || i == width - 1 || k == height - 1){
+					float num = MathUtils.reductiveNoise2d(noiseSeed, i, k, new float[]{8,4,2,1}, new float[]{4,3,2,1});
+					texMap.setColor(new Color(carpetColor.r*(0.6f+0.2f*num), carpetColor.g*(0.6f+0.2f*num), carpetColor.b*(0.6f+0.2f*num), 1);
+					
+				}else{
+					float num = MathUtils.reductiveNoise2d(noiseSeed, i, k, new float[]{8,4,2,1}, new float[]{4,3,2,1});
+					texMap.setColor(new Color(carpetColor.r*(0.9f+0.2f*num), carpetColor.g*(0.9f+0.2f*num), carpetColor.b*(0.9f+0.2f*num), 1);
+				}
+				texMap.drawPixel(i, height - 1 - k); //is this right?
 			}
 		}
 	}
 	
 	public void generateChecks(Pixmap texMap, int width, int height){
-		
+		Color color1 = new Color((float)(0.3f+0.3f*Math.random()), (float)(0.3f+0.3f*Math.random()), (float)(0.3f+0.3f*Math.random()), 1);
+		Color color2 = new Color((float)(0.3f+0.3f*Math.random()), (float)(0.3f+0.3f*Math.random()), (float)(0.3f+0.3f*Math.random()), 1);
 	}
 	
 	public void generateOrnimental(Pixmap texMap, int width, int height){
