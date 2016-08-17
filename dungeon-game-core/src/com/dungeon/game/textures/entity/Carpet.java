f87dce10b70eff8b8bbaaf99e5346a4d12bc651a
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-17 09:31:07 -0700

    Create Carpet.java

---
+++ dungeon-game-core/src/com/dungeon/game/textures/entity/Carpet.java
@@ -0,0 +1,9 @@
+package com.dungeon.game.textures.entity;
+
+public class Bookshelf extends ProceduralTexture {
+    public Bookshelf(int width, int height, int[][][] shelfMap) {
+		    super(16*width, 16*height, new Object[]{shelfMap});
+		
+	  }
+
+}
