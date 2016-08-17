5e590b3597da3b6d86edcbc40ec6a50e82d8b5e0
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-17 09:36:44 -0700

    Update Carpet.java

--- dungeon-game-core/src/com/dungeon/game/textures/entity/Carpet.java
+++ dungeon-game-core/src/com/dungeon/game/textures/entity/Carpet.java
@@ -1,9 +1,26 @@
 package com.dungeon.game.textures.entity;
 
-public class Bookshelf extends ProceduralTexture {
-    public Bookshelf(int width, int height, int[][][] shelfMap) {
-		    super(16*width, 16*height, new Object[]{shelfMap});
+public class Carpet extends ProceduralTexture {
+	
+	private static final int RANDOM = -1;
+	private static final int PLAIN = 0;
+	private static final int CHECKERED = 1;
+	private static final int ORNIMENTAL = 2;
+	private static final int ABSTRACT = 3;
+	
+	private int type;
+	
+	
+	public Carpet(int width, int height, int type){
+		super(16*width, 16*height, new Object[]{type});
 		
-	  }
+	}
+	
+	public void generateTexture(Object[] args) {
+		type = (int) args[0];
+		Pixmap texMap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+		
+		texture = new Texture(texMap)
+	}
 
 }
