2cc7acf654ffaffbcae63dd6f4b31cdc73195951
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-17 09:43:51 -0700

    Update Carpet.java

--- dungeon-game-core/src/com/dungeon/game/textures/entity/Carpet.java
+++ dungeon-game-core/src/com/dungeon/game/textures/entity/Carpet.java
@@ -12,15 +12,46 @@ public class Carpet extends ProceduralTexture {
 	
 	
 	public Carpet(int width, int height, int type){
-		super(16*width, 16*height, new Object[]{type});
+		super(16*width, 16*height, new Object[]{type, width, height});
 		
 	}
 	
 	public void generateTexture(Object[] args) {
 		type = (int) args[0];
 		Pixmap texMap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
-		
+		if(type == -1)type = (int)(Math.random()*4);
+		swtich(type){
+			case 0:
+				generatePlain(texMap, (int) args[1], (int) args[2]);
+			break;
+			case 1:
+				generateChecks(texMap, (int) args[1], (int) args[2]);
+			break;
+			case 2:
+				generateOrnimental(texMap, (int) args[1], (int) args[2]);
+			break;
+			case 3:
+				generateAbstract(texMap, (int) args[1], (int) args[2]);
+			break;
+				
+		}
 		texture = new Texture(texMap)
+	}
+	
+	public void generatePlain(Pixmap texMap, int width, int height){
+		Color carpetColor = new Color((float)(0.3f+0.3f*Math.random()),)
+	}
+	
+	public void generateChecks(Pixmap texMap, int width, int height){
+		
+	}
+	
+	public void generateOrnimental(Pixmap texMap, int width, int height){
+		
+	}
+	
+	public void generateAbstract(Pixmap texMap, int width, int height){
+		
 	}
 
 }
