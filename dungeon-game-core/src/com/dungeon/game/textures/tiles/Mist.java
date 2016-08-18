1dbe5972c28fdbaa4af8bd6b3c3c3ae49c9c1ef3
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-17 22:52:02 -0700

    started mist

--- dungeon-game-core/src/com/dungeon/game/textures/tiles/Mist.java
+++ dungeon-game-core/src/com/dungeon/game/textures/tiles/Mist.java
@@ -4,12 +4,15 @@ import com.dungeon.game.textures.AnimatedTexture;
 
 public class Mist extends ProceduralTile implements AnimatedTexture {
 	
-	private int timer;
+	private int timer; //timer for animations
+	
+	private int seed; //seed unique to this mist
+	
+	private int fadeEdges; //weather this mist should fade around the edges. (yes if the mist should fade in, no if it should not fade in OR cover the whole map)
 	
 	public Mist(int seed, int x, int y) {
 		super(new Object[]{seed, x, y});
 	}
-	
 	
 	public void generateTexture(Object[] args) {
 		timer = 0;
