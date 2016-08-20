d53e1979acc5331d8da32e706242b64a59848966
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-19 21:35:59 -0700

    torches dont spawn floaty!

--- dungeon-game-core/src/com/dungeon/game/generator/rooms/hallway/BasicHall.java
+++ dungeon-game-core/src/com/dungeon/game/generator/rooms/hallway/BasicHall.java
@@ -23,13 +23,62 @@ public class BasicHall extends Hallway {
 	    	if(count == 0) {
 	    		count = (int) Math.floor(Math.random()*2)+2;
 				int dir = cords[i][2];
+				//0 = down 1 == up 2 == left 3 == right
 				
-				if(dir == 0 || dir == 1) dir = Math.random()>0.5?0:2;
-				else dir = Math.random() >0.5?1:3;
+				if(i < cords.length - 1 && dir != cords[i+1][2]){
+					int dirNext = cords[i+1][2];
+					if(dir == 0){
+						if(dirNext == 2){
+							dir = 2;
+						}else {
+							dir = 0;
+						}
+					}else if(dir == 1){
+						if(dirNext == 2){
+							dir = 2;
+						}else {
+							dir = 0;
+						}
+					}else if(dir == 2){
+						if(dirNext == 0){
+							dir = 1;
+						}else {
+							dir = 3;
+						}
+					}else if(dir == 3){
+						if(dirNext == 0){
+							dir = 1;
+						}else {
+							dir = 3;
+						}
+					}
+				}else{
+					
+					if(dir == 0 || dir == 1) dir = Math.random()>0.5?0:2;
+					else dir = Math.random() >0.5?1:3;
+					
+				}
 				
 				
 				
-				entities.add(new Torch(world, cords[i][0]*Tile.TS+Tile.TS/2, cords[i][1]*Tile.TS + Tile.TS/2, dir)); //0 = 1, 1 = 3, 2 = 0, 3 = 2
+				//check if we need to change the dir because the next tile is a different dir
+
+				int x = cords[i][0]*Tile.TS+Tile.TS/2;
+				int y = cords[i][1]*Tile.TS+Tile.TS/2;
+				
+				if(dir == 0){
+					x -= Tile.TS/4;
+				}else if(dir == 1){
+					y += Tile.TS/4;
+				}else if(dir == 2){
+					x += Tile.TS/4;
+				}else if(dir == 3){
+					y -= Tile.TS/4;
+				}
+				
+				
+				
+				entities.add(new Torch(world, x, y, dir));
 	    	}
 	    }
 	}
