3cc96e924dc539ad92a1cd2514a1309faaa21625
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-28 14:52:17 -0700

    kept making the test

--- dungeon-game-desktop/src/com/dungeon/game/desktop/HierarchicalPathfindingTest.java
+++ dungeon-game-desktop/src/com/dungeon/game/desktop/HierarchicalPathfindingTest.java
@@ -2,17 +2,24 @@ package com.dungeon.game.desktop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ai.pfa.Connection;
+import com.badlogic.gdx.ai.pfa.indexed.IndexedHierarchicalGraph;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.dungeon.game.DungeonGame;
-import com.dungeon.game.GeneratorTest;
+import com.dungeon.game.pathing.Node;
 import com.dungeon.game.world.Tile;
-import com.dungeon.game.world.World;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.graphics.GL20;
+
 
 public class HierarchicalPathfindingTest extends ApplicationAdapter {
 	private ShapeRenderer shapeRenderer; //the shapeRenderer
@@ -43,7 +50,6 @@ public class HierarchicalPathfindingTest extends ApplicationAdapter {
 	
 	public static void main (String[] arg) { //MAIN METHOD
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-//		config.useGL30 = true;
 		new LwjglApplication(new HierarchicalPathfindingTest(), config);
 	}
 	
@@ -51,8 +57,133 @@ public class HierarchicalPathfindingTest extends ApplicationAdapter {
 	
 	private class Node{
 		
+		private Array<com.badlogic.gdx.ai.pfa.Connection<Node>> connections = new Array<com.badlogic.gdx.ai.pfa.Connection<Node>>();
+		public float cost;
+		private int index;
+		public float x;
+		public float y;
+		public Node upNode;
+		public Node downNode;
+		
+		public Node(float x, float y, float cost, int index){
+			this.index = index;
+			this.cost = cost;
+			this.x = x;
+			this.y = y;
+		}
+		
+
+		
+		public void makeConnection(Node end, float cost){
+			connections.add(new Connection(this, end, cost));
+		}
+		
+		public int getIndex(){
+			return index;
+		}
+		
+		public Array<com.badlogic.gdx.ai.pfa.Connection<Node>> getConnections(){
+			return connections;
+		}
+		
+		//returns the distance between this node and a given x and y
+		public float findDistance(float x, float y){
+			return (float) Math.sqrt((x-this.x*Tile.TS) * (x-this.x*Tile.TS) + (y-this.y*Tile.TS) * (y-this.y*Tile.TS));
+		}
 	}
 	
 	//implementations of Heirarchical Pathfinding classes
 	
+	//connection implementation
+	
+	private class Connection implements com.badlogic.gdx.ai.pfa.Connection<Node>{
+		private Node startNode;
+		private Node endNode;
+		private float cost;
+
+		
+		public Connection(Node startNode, Node endNode, float cost){
+			this.startNode = startNode;
+			this.endNode = endNode;
+			this.cost = cost;
+		}
+		
+		@Override
+		public float getCost() {
+			return cost;
+		}
+
+		@Override
+		public Node getFromNode() {
+			return startNode;
+		}
+
+		@Override
+		public Node getToNode() {
+			return endNode;
+		}
+		
+	}
+	
+	//heirarchicalGraph implementation
+	
+
+	public class HierarchicalGraph extends IndexedHierarchicalGraph<Node> {
+		
+		public Node[][] nodes;
+
+		public HierarchicalGraph(Node[][] nodes) {
+			super(nodes.length);
+			this.nodes = nodes;
+		}
+
+		@Override
+		public int getIndex(Node node) {
+			// TODO Auto-generated method stub
+			return node.index;
+		}
+
+		@Override
+		public int getNodeCount() {
+			return nodes[level].length;
+		}
+
+		@Override
+		public Array<com.badlogic.gdx.ai.pfa.Connection<Node>> getConnections(Node fromNode) {
+			// TODO Auto-generated method stub
+			return fromNode.getConnections();
+		}
+
+		@Override
+		public Node convertNodeBetweenLevels(int inputLevel, Node node, int outputLevel) {
+			if(inputLevel == outputLevel)return node;
+			else if(inputLevel > outputLevel) return node.downNode;
+			else return node.upNode;
+		}
+		
+		//finds the closest node on the current graph level to this position
+		public Node getClosestNode(float x, float y){
+			Node closeNode = nodes[level][0];
+			float closeNodeDist = closeNode.findDistance(x, y);
+			for(Node n: nodes[level]){
+				if(n.findDistance(x, y) < closeNodeDist){
+					closeNode = n;
+					closeNodeDist = n.findDistance(x, y);
+				}
+			}
+			return closeNode;
+		}
+	
+	}
+	
+	//heuristic implementation
+	
+	private class Heuristic implements com.badlogic.gdx.ai.pfa.Heuristic<Node> {
+
+		@Override
+		public float estimate(Node node, Node endNode) {
+			return 0; //for now we don't care about optimization, so the Hueristic will always return 0;
+		}
+	
+	}
 }
