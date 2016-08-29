0e6e22e8d957b5da7ad76d7701631a38a6a76b4f
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-08-29 12:34:24 -0700

    continued tutorial

--- dungeon-game-core/src/com/dungeon/game/generator/TutorialGenerator.java
+++ dungeon-game-core/src/com/dungeon/game/generator/TutorialGenerator.java
@@ -3,10 +3,12 @@ package com.dungeon.game.generator;
 import java.util.ArrayList;
 
 import com.badlogic.gdx.math.Rectangle;
-import com.dungeon.game.entity.furniture.Door;
+import com.dungeon.game.entity.Drop;
 import com.dungeon.game.entity.furniture.LockedDoor;
 import com.dungeon.game.generator.rooms.room.EmptyRoom;
 import com.dungeon.game.generator.rooms.room.Room;
+import com.dungeon.game.inventory.Slot;
+import com.dungeon.game.item.Key;
 import com.dungeon.game.pathing.HierarchicalGraph;
 import com.dungeon.game.pathing.Node;
 import com.dungeon.game.world.Tile;
@@ -20,16 +22,33 @@ public class TutorialGenerator extends Generation {
 	
 	public void generate(Object[] args){
 		super.generate(args);
+		Rectangle workingRoom;
+
+		workingRoom = new Rectangle(48, 52, 1, 1);
+		addToMap(workingRoom);
+		entities.add(new LockedDoor(world, 48, 52, 0));
 		
-		Rectangle startRoom = new Rectangle(43, 43, 5, 5);
-		addToMap(startRoom);
-		map[43][42] = tileMap.getTile(0);
-		entities.add(new LockedDoor(world, 42, 43, 1));
-		entities.add(new Door(world, 43, 43, 1));
-		
-		Room startRoomRoom = new EmptyRoom(world, startRoom, findDoors(startRoom), tileMap);
+		workingRoom = new Rectangle(45, 45, 7, 7);
+		addToMap(workingRoom);
+		Room startRoomRoom = new EmptyRoom(world, workingRoom, findDoors(workingRoom), tileMap);
 		startRoomRoom.addToMap(map, entities);
 		
+		workingRoom = new Rectangle(30, 53, 23, 2);
+		addToMap(workingRoom);
+
+		workingRoom = new Rectangle(30, 55, 5, 8);
+		addToMap(workingRoom);
+		Slot s = new Slot(world, new int[]{0, 0, 0}, null);
+		s.item = new Key(world);
+		entities.add(new Drop(world, 32*Tile.TS+Tile.TS/2, 59*Tile.TS+Tile.TS/2, s));
+		
+		workingRoom = new Rectangle(32, 63, 1, 1);
+		addToMap(workingRoom);
+		entities.add(new LockedDoor(world, 32, 63, 2));
+		
+
+		workingRoom = new Rectangle(30, 64, 20, 2);
+		addToMap(workingRoom);
 		
 		makeWalls(10, 11, 12, 13, 14);
 		
