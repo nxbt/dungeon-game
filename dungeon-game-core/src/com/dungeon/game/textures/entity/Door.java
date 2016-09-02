0c5fc04fc79719e4428c0e76a9945b7fd43d929b
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-09-02 15:33:28 -0700

    revamped locked door sprite

--- dungeon-game-core/src/com/dungeon/game/textures/entity/Door.java
+++ dungeon-game-core/src/com/dungeon/game/textures/entity/Door.java
@@ -16,16 +16,21 @@ public class Door extends ProceduralTexture {
 	public void generateTexture(Object[] args) {
 		Pixmap texMap = new Pixmap(32, 8,Pixmap.Format.RGBA8888); //create pixmap
 		Color doorColor = new Color(163f / 255f, 60f / 255f, 8f / 255f, 1);
-		if((Boolean) args[1]){
-			doorColor.mul(0.7f);
-			doorColor.a = 1;
-		}
 		texMap.setColor(doorColor);
 		texMap.fillRectangle(0, 0, 32, 8);
 		texMap.setColor(new Color(doorColor.r*0.8f, doorColor.g*0.8f, doorColor.b*0.8f, 1));
 		texMap.drawRectangle(0, 0, 32, 8);
+		
+			texMap.setColor(Color.BLACK);
+			texMap.drawLine(13, 6, 18, 6);
+			texMap.drawLine(14, 5, 17, 5);
+			texMap.drawLine(15, 4, 16, 4);
+			texMap.drawLine(14, 3, 17, 3);
+			texMap.drawLine(14, 2, 17, 2);
+			texMap.drawLine(15, 1, 16, 1);
+		}
+		
 		texture = new Texture(Spritesheet.rotatePixmap(texMap, (Integer)(args[0])));
-
 	}
 
 }
