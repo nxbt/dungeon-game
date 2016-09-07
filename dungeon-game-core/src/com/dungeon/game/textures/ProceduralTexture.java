310269c719cd64b65e9288d6709b916269d842df
Petjanh <ebull-vulpe.sh@mysummitps.org>
2016-09-07 09:25:31 -0700

    tried to optimize texture generation

--- dungeon-game-core/src/com/dungeon/game/textures/ProceduralTexture.java
+++ dungeon-game-core/src/com/dungeon/game/textures/ProceduralTexture.java
@@ -11,9 +11,11 @@ public abstract class ProceduralTexture {
 	public int height;
 	
 	public ProceduralTexture(int width, int height, Object[] args){
+		long startNanoTime = System.nanoTime();
 		this.width = width;
 		this.height = height;
 		generateTexture(args);
+		System.out.println(((float)(System.nanoTime() - startNanoTime))/16000000f + " frames to generate a: " + this.getClass().getSimpleName());
 	}
 	
 	public abstract void generateTexture(Object[] args);
